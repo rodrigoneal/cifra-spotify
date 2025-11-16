58220d17afe45dfec8fb7342d6de59380dd76cef
rodrigoneal <rodrigho2006@gmail.com>
2025-11-15 22:12:52 -0300

    WIP: Gerando o PDF da Cifra

---
+++ src/cifra_spotify/api/router/cifra.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/api/cifra", tags=["CIFRA"])
+
+
+@router.get("/")
+async def cifra():
+	return
--- tests/test_cifra.py
+++ tests/test_cifra.py
@@ -1,10 +1,26 @@
+from pathlib import Path
 import pytest
 
 from src.cifra_spotify.cifras.cifra_base import Instruments
 from src.cifra_spotify.cifras.cifra_club import CifraClub
 
+
+
+
 @pytest.mark.asyncio
 async def test_se_pega_a_cifra_cifra_club():
     cifra_club = CifraClub()
-    cifra = await cifra_club.get_cifra("Arlindo Cruz", "Trilha do Amor", Instruments.GUITAR)
-    assert cifra
+    cifra = await cifra_club.get_cifra(
+        "Arlindo Cruz", "Trilha do Amor", Instruments.GUITAR
+    )
+    assert "A#m6" in cifra["cifra"]
+
+
+
+@pytest.mark.asyncio
+async def test_se_pega_a_cifra_pdf():
+    cifra_club = CifraClub()
+    cifra = await cifra_club.to_pdf(
+        "Arlindo Cruz", "Trilha do Amor", Instruments.CAVACO, tabs=False
+    )
+    assert cifra.startswith(b"%PDF-1.7")
